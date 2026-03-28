4328416b9ce49aaac8f0969dcc52cf99ebb682d0
Gaurav-kangane <163861574+Gaurav-kangane@users.noreply.github.com>
2026-03-28 22:29:04 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -7,9 +7,9 @@ CORS(app)
 
 # Database Configuration
 db_config = {
-    'host': 'book-rds.cinsoscgioqa.us-east-1.rds.amazonaws.com',
+    'host': 'database-1.ct2emgmy8qpw.ap-south-1.rds.amazonaws.com',
     'user': 'admin',
-    'password': 'SuperSecretPass123',
+    'password': 'Cloud123',
     'database': 'dev'
 }
 
